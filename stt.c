b9e30ef71e8f1c6cc4aed70bd969e1080cac6854
AnkitaKumari15 <43900609+AnkitaKumari15@users.noreply.github.com>
2019-07-20 18:37:37 +0530

    Add files via upload

---
+++ stt.c
@@ -0,0 +1,51 @@
+//Implementation in stack using array!!!
+#include<stdio.h>
+# define max 100
+int A[max];
+int top=-1;
+void push(int x)
+{ 
+ if(top==max-1)
+  {
+    printf("Overflow");
+    return;
+  }
+   top=top+1;
+   A[top]=x;
+}
+void pop()
+ {
+   if(top==-1)
+  {
+    printf("underflow");
+    return;
+  }
+   top=top-1;
+ }
+int Top()
+{ 
+  return A[top];
+}
+Print(){
+printf("stack");
+for(int i=0;i<=top;i++)
+printf("%d",A[i]);
+printf("\n");
+}
+int main()
+{
+push(1);
+push(2);
+push(3);
+push(4);
+Print();
+pop();
+pop();
+pop();
+pop();
+
+push(5);
+Print();
+return 0;
+}
+ 
